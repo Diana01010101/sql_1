5c76be61d2db4b6ed9a0d1738eccf03c9a91783d
Diana test <diana.kapralova@mail.ru>
2024-06-24 11:56:09 +0300

    initial commit

---
+++ src/test/java/test/BankLoginTest.java
@@ -0,0 +1,71 @@
+package ru.netology.banklogin.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import ru.netology.banklogin.data.DataHelper;
+import ru.netology.banklogin.data.SQLHelper;
+import ru.netology.banklogin.page.LoginPage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.banklogin.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.banklogin.data.SQLHelper.cleanDatabase;
+public class BankLoginTest {
+
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+        Configuration.browserCapabilities = options;
+
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login and password from sut test data")
+    void shouldSuccessfulLogin() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if use is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithoutAddingToBase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login with exist in base and active user and random verification code")
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifiErrorNotification("Ошибка! Неверно указан код! \nПопробуйте ещё раз.");
+    }
+
+}
